0be9159f7e692fb9fb99a3405dcdf07bfd9dbf3e
Lushangxing <142703747+cexzreal@users.noreply.github.com>
2024-10-24 17:00:41 +0800

    Update auto_ins_kcp.sh

--- auto_ins_kcp.sh
+++ auto_ins_kcp.sh
@@ -2,6 +2,7 @@
 
 curl -L https://github.com/xtaci/kcptun/releases/download/v20240919/kcptun-linux-amd64-20240919.tar.gz -0 kcp.tar.gz
 ssPort=$(jq -r '.server_port' /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json)
+ipaddr=$(curl -s http://checkip.amazonaws.com)
 
 tar -zxvf kcp.tar.gz
 
@@ -24,20 +25,59 @@ cat<<EOF>/usr/local/etc/kcpser/config.json
         "dscp": 0,
         "nocomp": true,
         "acknodelay": false,
-        "nodelay": 1,
-        "interval": 20,
-        "resend": 2,
-        "nc": 1,
         "sockbuf": 16777217,
-        "smuxbuf": 16777217,
-        "streambuf":4194304,
         "keepalive": 10,
-        "pprof":false,
         "quiet":false,
         "tcp":false
 }
 EOF
 
-echo "use configs below to enable kcp on Android:"
+cat<<EOF>/usr/local/etc/kcpser/cilconf.config
+{
+        "listen": ":$(shuf -i 33666-34690 -n 1)", 
+        "target": "$ipaddr:$ssPort", 
+        "key": "pubgpubg", 
+        "crypt": "twofish",
+        "mode": "fast3",
+        "mtu": 1400,
+        "sndwnd": 2048,
+        "rcvwnd": 2048,
+        "datashard": 10,
+        "parityshard": 0,
+        "dscp": 0,
+        "nocomp": true,
+        "acknodelay": false,
+        "sockbuf": 16777217,
+        "keepalive": 10,
+        "quiet":false,
+        "tcp":false
+}
 
-kcpconf=""
+cat<<EOF>/etc/supervisor/conf.d/game.conf
+[program:kcptun]
+command=/usr/local/bin/kcptun -c /usr/local/etc/kcpser/config.json
+autostart=true
+autorestart=true
+redirect_stderr=false
+
+EOF
+
+supervisorctl update
+
+
+echo "kcptun has been set on ur device"
+
+echo "cilent config are as belows:"
+cat /usr/local/etc/kcpser/cilconf.config
+echo '''usage are as belows:
+    1. copy it to ur clipboard
+    2. create a jsonfile and paste it to jsonfile
+    3. save and import it to ur kcp gui
+    4. or use cmd cilent-amd64.exe -c jsonfile path
+'''
+
+kcpconf="key=pubgpubg;mtu=1400;rcvwnd=2048;sndwnd=2048;datashard=10;parityshard=0;mode=fast3;sockbuf=16777217;dscp=0;crypt=twofish;nocomp"
+echo "android kcp can use parameters below:"
+echo $kcpconf
+
+exit
