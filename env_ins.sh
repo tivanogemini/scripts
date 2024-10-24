907ae8fa5b86bb30a00f5863672717fcb5a04ae2
Lushangxing <142703747+cexzreal@users.noreply.github.com>
2024-10-24 17:03:02 +0800

    Update env_ins.sh

--- env_ins.sh
+++ env_ins.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+echo ""
+
 apt-update -y
 apt-upgrade -y
 apt remove iptables* -y
@@ -12,3 +14,40 @@ apt install snap -y
 
 snap install core
 
+apt install supvervisor -y
+
+systemctl enable --now supervisor
+
+echo "
+    1. auto install ss without any parameter config
+    2. manually install ss
+    3. install kcptun, default bind to ss port
+    4. install udp speeder
+"
+
+read -p "choose plan to install:" pj
+
+case $pj in
+    1)
+        echo "start to install ss automatically"
+        bash auto_ins_ss.sh
+        ;;
+    2)
+        echo "start to install ss manually"
+        bash manu_ins_ss.sh
+        ;;
+    3)
+        echo "start to install kcp"
+        bash auto_ins_kcp.sh
+        ;;
+    4)
+        echo "start to install udpspeeder"
+        bash auto_ins_usp.sh
+        ;;
+    *)
+        echo "invalid input, check it"
+        exit
+        ;;
+esac
+
+exit
